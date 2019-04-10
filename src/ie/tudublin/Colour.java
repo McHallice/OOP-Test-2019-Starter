4ee3067ab50ae0674a06daa16b9f4ebaf6e2cdf3
Ciaran McHale <ciaranmch7@gmail.com>
2019-04-10 17:52:09 +0000

    Added accessor methods and a constructer

--- src/ie/tudublin/Colour.java
+++ src/ie/tudublin/Colour.java
@@ -9,7 +9,76 @@ public class Colour
     public int r;
     public int g;
     public int b;
+
+    /**
+     * @return the colour
+     */
+    public String getColour() {
+        return colour;
+    }
+
+    /**
+     * @param colour the colour to set
+     */
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    /**
+     * @return the r
+     */
+    public int getR() {
+        return r;
+    }
+
+    /**
+     * @param r the r to set
+     */
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    /**
+     * @return the g
+     */
+    public int getG() {
+        return g;
+    }
+
+    /**
+     * @param g the g to set
+     */
+    public void setG(int g) {
+        this.g = g;
+    }
+
+    /**
+     * @return the b
+     */
+    public int getB() {
+        return b;
+    }
+
+    /**
+     * @param b the b to set
+     */
+    public void setB(int b) {
+        this.b = b;
+    }
     
+    public String toString()
+    {
+        return colour + "\t" + r + "\t" + g + "\t" + b;  
+    }
+
+    public Colour(TableRow row)
+    {
+        colour = row.getString("colour");
+        r = row.getInt("r");
+        g = row.getInt("g");
+        b = row.getInt("b");
+        
+    }
 
 
 
